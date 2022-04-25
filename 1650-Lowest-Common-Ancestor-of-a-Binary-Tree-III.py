67e39e1b000c8c3935f60d3cced07b1f19829c45
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-04-24 22:05:51 -0400

    Update 1650-Lowest-Common-Ancestor-of-a-Binary-Tree-III.py

--- 1650-Lowest-Common-Ancestor-of-a-Binary-Tree-III.py
+++ 1650-Lowest-Common-Ancestor-of-a-Binary-Tree-III.py
@@ -8,6 +8,9 @@ class Node:
         self.parent = None
 """
 
+"""
+METHOD 1
+"""
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         p_list = []
@@ -30,3 +33,35 @@ class Solution:
             else:
                 return prev
         return prev
+
+"""
+METHOD 2
+"""
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        root = p
+        while root:
+            if root.parent is None:
+                break
+            root = root.parent
+        return self.dfs(root, p, q)
+        
+    def dfs(self, root, p, q):
+        
+        if root is None:
+            return None
+        
+        if root.val == p.val or root.val == q.val:
+            return root
+        
+        lca_left  = self.dfs(root.left , p, q)
+        lca_right = self.dfs(root.right, p, q)
+        
+        if lca_left and lca_right:
+            return root
+        
+        if lca_left and not lca_right:
+            return lca_left
+        
+        if not lca_left and lca_right:
+            return lca_right
