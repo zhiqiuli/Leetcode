2b6e661642bdf13bbd04967567111045f6292daa
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-04-24 22:12:07 -0400

    Update 1650-Lowest-Common-Ancestor-of-a-Binary-Tree-III.py

--- 1650-Lowest-Common-Ancestor-of-a-Binary-Tree-III.py
+++ 1650-Lowest-Common-Ancestor-of-a-Binary-Tree-III.py
@@ -13,27 +13,14 @@ METHOD 1
 """
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        p_list = []
+        path = set()
         while p:
-            p_list.append(p)
+            path.add(p)
             p = p.parent
-        q_list = []
-        while q:
-            q_list.append(q)
+        while q not in path:
             q = q.parent
-        if p_list[-1].val != q_list[-1].val:
-            return root
-        prev = p_list[-1]
-        i, j = len(p_list) - 2, len(q_list) - 2
-        while i >= 0 and j >= 0:
-            if p_list[i].val == q_list[j].val:
-                prev = p_list[i]
-                i -= 1
-                j -= 1
-            else:
-                return prev
-        return prev
-
+        return q
+    
 """
 METHOD 2
 """
