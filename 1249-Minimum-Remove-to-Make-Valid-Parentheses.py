c77ec9c1226436f0492d79ee2ecc28ac43d21571
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-04-24 23:31:55 -0400

    Update 1249-Minimum-Remove-to-Make-Valid-Parentheses.py

--- 1249-Minimum-Remove-to-Make-Valid-Parentheses.py
+++ 1249-Minimum-Remove-to-Make-Valid-Parentheses.py
@@ -1,8 +1,8 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         stack = []
-        extra_left  = []
-        extra_right = []
+        extra_left  = [] # 记录多余的(的位置
+        extra_right = [] # 记录多余的)的位置
         for i, c in enumerate(s):
             if c == '(':
                 stack.append(i)
