0dafda639472b6317dd6954e5fa760e70e47544d
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-07-23 11:01:54 -0400

    update some sql

--- SQL/178-Rank-Scores.sql
+++ SQL/178-Rank-Scores.sql
@@ -1,2 +1,52 @@
 SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS 'RANK'
 FROM Scores
+
+/*
+DENSE_RANK() and RANK()
+
+Syntax:
+
+DENSE_RANK() OVER (
+    [PARTITION BY partition_expression, ... ]
+    ORDER BY sort_expression [ASC | DESC], ...
+)
+
+Example:
+
+SELECT
+	v,
+	DENSE_RANK() OVER (
+		ORDER BY v
+	) my_dense_rank,
+	RANK() OVER (
+		ORDER BY v
+	) my_rank
+FROM
+	sales.dense_rank_demo;
+
+v my_dense_rank my_rank
+A 1             1
+B 2             2
+B 2             2
+C 3             4
+C 3             4
+D 4             6
+E 5             7
+
+More complicated with PARTITION
+
+SELECT * FROM (
+	SELECT
+		product_id,
+		product_name,
+		category_id,
+		list_price,
+		DENSE_RANK () OVER ( 
+			PARTITION BY category_id
+			ORDER BY list_price DESC
+		) price_rank 
+	FROM
+		production.products
+) t
+WHERE price_rank < 3;
+*/
--- SQL/184-Department-Highest-Salary.sql
+++ SQL/184-Department-Highest-Salary.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+-- Option 1
 select d.name as Department, e.name Employee, salary as Salary
 from Department d, Employee e
 where d.id = e.departmentId -- 相当于是join table了，这种情况一定会match的话，可以同时select两个table然后
@@ -8,3 +8,13 @@ select departmentId, max(salary)
 from Employee
 group by departmentId
 )
+
+-- Option 2
+select d.name as Department, e.name as Employee, m.max_salary as Salary from
+(select departmentId, max(salary) as max_salary
+from Employee
+group by departmentId) m
+left join Employee e
+on e.salary = m.max_salary and e.departmentId = m.departmentId
+left join Department d
+on d.id = e.departmentId
