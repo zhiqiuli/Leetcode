9b0651cf180906d9efdcf6a51b114663f0e08f27
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-08-01 21:44:02 -0400

    Update 608-Tree-Node.sql

--- SQL/608-Tree-Node.sql
+++ SQL/608-Tree-Node.sql
@@ -1,5 +1,7 @@
-select id,
-case when p_id is null then 'Root'
-     when id in (select p_id from tree) then 'Inner'
-     else 'Leaf' end as Type
-from tree
+/* Write your T-SQL query statement below */
+select id, case
+               when p_id is null then 'Root'
+               when p_id is not null and id in (select distinct p_id from Tree) then 'Inner'
+               else 'Leaf'
+           end as type
+from Tree
