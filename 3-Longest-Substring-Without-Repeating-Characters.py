116c4ce31d26041c65ff851d0c7ed2674050693d
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2026-07-05 19:52:14 -0400

    Update 3-Longest-Substring-Without-Repeating-Characters.py

--- 3-Longest-Substring-Without-Repeating-Characters.py
+++ 3-Longest-Substring-Without-Repeating-Characters.py
@@ -1,13 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s: return 0
-        index = {}
-        i = 0
-        index[s[i]] = i
-        res = 1
-        for j in range(1, len(s)):
-            if s[j] in index:
-                i = max(i, index[s[j]] + 1) # remember that i only moves to the right, so the max is needed!
-            index[s[j]] = j
-            res = max(res, j - i + 1)
-        return res
+        if not s:
+            return 0
+        
+        # 经典双指针问题
+        last_seen = {}
+        left = 0
+        longest = 0
+
+        for right, char in enumerate(s):
+            if char in last_seen: # 先检查char是否已经存在在扫描过的数组中
+                left = max(left, last_seen[char] + 1) # 如果存在的话，使用lastseen右+1的位置作为substring开始
+            
+            last_seen[char] = right # 无论如何都需要更新lastSeen
+            longest = max(longest, right - left + 1) # 无论如何都更新答案
+
+        return longest
